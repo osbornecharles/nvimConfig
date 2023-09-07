196d82f0dc50b853989a2053005fedf24edc5b51
Charlie Osborne <cosborne0731@LAPTOP-C5F35MK7.localdomain>
2023-09-07 20:32:48 +0800

    config updates

---
+++ after/ftplugin/lua/lua.lua
@@ -0,0 +1,12 @@
+-- Enable break indent
+vim.o.breakindent = true
+vim.opt.breakindentopt = { "min:30", "sbr" }
+vim.o.linebreak = true
+vim.o.showbreak = '>> '
+vim.o.breakat = ' \t;,!?./'
+
+-- Tab settings
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
---
+++ lua/custom/plugins/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+-- [[ Configure Nvim-Autopairs ]]
+-- See `:help nvim-autopairs`
+
+return {
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	opts = {} -- this is equivalent to setup({}) function
+}
